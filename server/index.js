d9eca79e2bad395bfe7eefb88cfb80f28e1fc51d
ishii makoto <makotunn0714@icloud.com>
2025-04-02 22:30:54 +0900

    変更

--- server/index.js
+++ server/index.js
@@ -14,12 +14,20 @@ app.use(cors({
 app.use(express.json());
 app.use("/api/v1",require("./src/v1/routes"));
 
-
+// mongoose.connect(process.env.MONGODB_URL,{
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true,
+// })
+// .then(()=>console.log("DBと接続中"))
+// .catch(error=>console.error("mongodb接続エラー",error));
 
 
 //DB接続
 try{
-    mongoose.connect(process.env.MONGODB_URL);
+    
+    mongoose.connect(process.env.MONGO_URI);
+    console.log(process.env.MONGO_URI);
+
     console.log("DBと接続中");
 }catch(error){
     console.log(error);
